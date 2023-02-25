55b120cb6852d60f3ee1aa120781decbe254fbe9
ExoskyCode <exoskyv2@gmail.com>
2023-02-25 15:24:07 +0100

    delete a todo feature

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,15 +1,14 @@
-import React from "react";
-import Link from "next/link";
-import styles from '@/styles/Nav.module.scss'
+import React from 'react';
+import Link from 'next/link';
+import styles from '@/styles/Nav.module.scss';
 
 export default function Navbar() {
-  return (
-    <div className={styles.containerNav}>
-      <ul>
-        <Link href="/">HOME</Link>
-        <Link href="/todo">ACTIVE TODO</Link>
-        <Link href="/oldTodo">OLD TODO</Link>
-      </ul>
-    </div>
-  );
+	return (
+		<div className={styles.containerNav}>
+			<ul>
+				<Link href='/'>HOME</Link>
+				<Link href='/todo'>ACTIVE TODO</Link>
+			</ul>
+		</div>
+	);
 }
--- components/Todo/TodosList.tsx
+++ components/Todo/TodosList.tsx
@@ -1,14 +1,26 @@
+import { useState } from 'react';
 import styles from '../../src/styles/todo.module.scss';
 
 interface Props {
 	todo: string;
-	uuid: string | number;
+	uuid: string;
 }
 
 export const TodosList = (props: Props) => {
+	const [handleTodo, setHandleTodo] = useState(true);
+
+	const deleteTodo = () => {
+		setHandleTodo(false);
+	};
+
 	return (
-		<div className={styles.TodosList}>
-			<p>{props.todo}</p>
+		<div className={styles.TodosListContainer}>
+			{handleTodo && (
+				<div key={props.uuid} className={styles.Todo}>
+					<p>{props.todo}</p>
+					<button onClick={deleteTodo}>Supprimer la todo</button>
+				</div>
+			)}
 		</div>
 	);
 };
